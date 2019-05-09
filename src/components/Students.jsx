f6108253e83b124c2d64d7678988d8c756cd2c2c
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-05-09 17:42:21 +0200

    Add student details

--- src/components/Students.jsx
+++ src/components/Students.jsx
@@ -9,7 +9,7 @@ const Students = ({ students }) => {
   const renderStudent = student => (
     <li key={student.id}>
       <button type="button" className="button--seamless" onClick={() => selectStudent(student)}>
-        <Pane className="students__student" elevation={1}>
+        <Pane className="students__student" elevation={2}>
           {student.firstName} {student.lastName}
         </Pane>
       </button>
@@ -26,14 +26,37 @@ const Students = ({ students }) => {
 
   return (
     <div className="container">
-      <ol className="students">
-        {$students}
-      </ol>
+      <ol className="students">{$students}</ol>
       <main className="student-detail">
-        Select a student from the list to display their information.
+        {console.log(selectedStudent)}
+        {!selectedStudent && 'Select a student from the list to display their information.'}
+        {selectedStudent && (
+          <Pane elevation={2} className="student-detail--infos">
+            <div>
+              <h3>Why do you want to join Open Summer of Code? </h3>
+              {selectedStudent.motivation}
+            </div>
+            <div>
+              <h3>Tell us why you think you&apos;ll make a good fit? </h3>
+              {selectedStudent.whyGoodFit}
+            </div>
+            <div>
+              <h3>What skills can you best help your teammates with? </h3>
+              {selectedStudent.bestSkills}
+            </div>
+            <div>
+              <h3>What would you like to learn or do better at Osoc? </h3>
+              {selectedStudent.learnOrDoBetter}
+            </div>
+            <div>
+              <h3>Links</h3>
+              <p>CV: {selectedStudent.CV}</p>
+              <p>Portfolio: {selectedStudent.portfolio}</p>
+            </div>
+          </Pane>
+        )}
       </main>
     </div>
-
   );
 };
 
--- src/models/Student.js
+++ src/models/Student.js
@@ -6,7 +6,8 @@ class Student {
     this.id = student.response_id;
   }
 
-  capitalizeName = name => name.split(' ')
+  capitalizeName = name => name
+    .split(' ')
     .map(s => s.charAt(0).toUpperCase() + s.substring(1))
     .join(' ');
 
@@ -18,6 +19,38 @@ class Student {
   get lastName() {
     const name = this.answers[1].text.trim().toLowerCase();
     return this.capitalizeName(name);
+  }
+
+  get motivation() {
+    return this.answers[12].text;
+  }
+
+  get whyGoodFit() {
+    return this.answers[13].text;
+  }
+
+  get bestSkills() {
+    return this.answers[14].text;
+  }
+
+  get learnOrDoBetter() {
+    return this.answers[15].text;
+  }
+
+  get roles() {
+    return this.answers[22].choices.labels;
+  }
+
+  get prevParticipation() {
+    return this.answers[21].choice.label;
+  }
+
+  get CV() {
+    return this.answers[10].url;
+  }
+
+  get portfolio() {
+    return this.answers[11].url;
   }
 }
 
