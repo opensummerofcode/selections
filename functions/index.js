febb0ff9cc70fe722c26ee5870a5b7b18ab36167
Michiel Leyman <michiel.leyman@gmail.com>
2019-05-18 20:58:49 +0200

    Add onCreate function for users

---
+++ functions/index.js
@@ -0,0 +1,15 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+
+exports.createUser = functions.auth.user().onCreate((user) => {
+  const newUser = {
+    email: user.email,
+    displayName: user.displayName,
+    uid: user.uid,
+    admin: false,
+    pending: true
+  };
+  return admin.firestore().collection('users').doc(user.uid).set(newUser);
+});
