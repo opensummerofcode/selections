2db0eb1aeb07adc46b09470ba51fc798ab9d3a9c
Michiel Leyman <michiel.leyman@gmail.com>
2019-06-05 23:42:35 +0200

    Clarify pending status

--- src/components/Pending.jsx
+++ src/components/Pending.jsx
@@ -3,18 +3,23 @@ import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 
 import AuthContext from '../context/auth';
+import logo from '../assets/osoc.png';
 
 const Pending = ({ history }) => {
   const { user, isLoading } = useContext(AuthContext);
   if (isLoading) return <p />;
   if (user && (!user.pending && ('pending' in user))) history.push('/');
   return (
-    <div>
+    <div className="pending">
       <h2>Hey {user && user.displayName} :)</h2>
-      {!user && <p>Please log in via the pop-up window</p>}
+      {!user && <p>Please log in via the pop-up window &amp; refresh the page</p>}
       {user && (user.pending || !('pending' in user)) && (
-        <p>Your account is pending. Shoot @michiel a message to start selecting students.</p>
+        <p>
+          For security reasons, your account is pending.
+          Shoot @michiel a message on Slack to start selecting students.
+        </p>
       )}
+      <div className="logo-container"><img src={logo} alt="Open Summer of Code logo" /></div>
     </div>
   );
 };
