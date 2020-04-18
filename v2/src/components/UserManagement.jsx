e4fb7efcc1def7a34f90c1add9d7ad9181782156
Michiel Leyman <michiel.leyman@gmail.com>
2020-04-18 22:17:08 +0200

    Make roles settable via user management module

--- v2/src/components/UserManagement.jsx
+++ v2/src/components/UserManagement.jsx
@@ -1,22 +1,105 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
+import { Table, Select } from 'evergreen-ui';
+import { User } from '../models';
 import { db } from '../firebase';
 
-const UserManagement = () => {
+import styles from '../assets/styles/user-management.module.css';
+
+const UserManagement = ({ user: currentUser }) => {
+  const [isLoading, setIsLoading] = useState(true);
   const [users, setUsers] = useState({});
+  const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
     const unsubscribe = db.collection('users').onSnapshot((snapshot) => {
       const newUsers = {};
       snapshot.forEach((doc) => {
         const data = doc.data();
-        newUsers[data.uid] = data;
+        // Don't show currently logged in user
+        if (currentUser.id !== data.uid) {
+          newUsers[data.uid] = new User(data);
+          if (isLoading) setIsLoading(false);
+        }
       });
       setUsers(newUsers);
     });
     return unsubscribe;
   }, []);
 
-  return <p>Hi</p>;
+  const doSearch = (value) => setSearchQuery(value);
+
+  const renderEmptyState = () => {
+    return <p>There are no users yet! Other than you of course :)</p>;
+  };
+
+  const setRole = (id, val) => {
+    console.log(val);
+    const roles = {
+      admin: val === 'admin',
+      pending: val === 'pending'
+    };
+    db.collection('users')
+      .doc(id)
+      .update({ ...roles });
+  };
+
+  const renderUser = (user) => {
+    return (
+      <Table.Row key={user.uid}>
+        <Table.TextCell>{user.name}</Table.TextCell>
+        <Table.TextCell>{user.email}</Table.TextCell>
+        <Table.TextCell>
+          <Select
+            width="100%"
+            defaultValue={user.role}
+            onChange={(e) => setRole(user.uid, e.currentTarget.value)}
+          >
+            <option value="admin">Administrator</option>
+            <option value="coach">Coach</option>
+            <option value="pending">Disabled</option>
+          </Select>
+        </Table.TextCell>
+      </Table.Row>
+    );
+  };
+
+  const filteredUsers = Object.keys(users).filter((id) => users[id].name.includes(searchQuery));
+
+  const renderUsers = () => {
+    const $users = filteredUsers.map((id) => renderUser(users[id]));
+    return (
+      <Table>
+        <Table.Head>
+          <Table.SearchHeaderCell onChange={doSearch} placeholder="Search names..." />
+          <Table.TextHeaderCell>Email</Table.TextHeaderCell>
+          <Table.TextHeaderCell>Account status</Table.TextHeaderCell>
+        </Table.Head>
+        <Table.Body height={240}>{$users}</Table.Body>
+      </Table>
+    );
+  };
+
+  if (!currentUser) {
+    if (!currentUser.isAdmin) return <Redirect to="/" />;
+  }
+
+  if (isLoading) return <div className="page-container" />;
+
+  const $content = Object.keys(users).length === 0 ? renderEmptyState() : renderUsers();
+  return (
+    <div className="page-container">
+      <main>
+        <h2>Manage users</h2>
+        {$content}
+      </main>
+    </div>
+  );
+};
+
+UserManagement.propTypes = {
+  user: PropTypes.instanceOf(User)
 };
 
 export default UserManagement;
--- v2/src/models/User.js
+++ v2/src/models/User.js
@@ -1,9 +1,17 @@
 class User {
   constructor(user) {
     this.id = user.uid;
+    this.uid = user.uid;
     this.name = user.displayName;
+    this.email = user.email;
     this.isPending = user.pending;
     this.isAdmin = user.admin;
+  }
+
+  get role() {
+    if (this.isAdmin) return 'admin';
+    if (!this.isPending) return 'coach';
+    return 'disabled';
   }
 }
 
