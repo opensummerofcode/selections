37078e072c1389cf3717fd5ae7ebf84d4d054984
Michiel Leyman <michiel.leyman@gmail.com>
2020-04-20 16:06:02 +0200

    Rework get/set paradigm

--- v2/src/models/Student.js
+++ v2/src/models/Student.js
@@ -1,143 +1,59 @@
+const parseField = (value, other = false) => {
+  if (typeof value === 'object' && value !== null) {
+    if (other) return value.other;
+    return value.answer;
+  }
+  return value;
+};
+
+const parseGender = (value) => parseField(value) || parseField(value, true);
+
+const parseNameOfSchool = (school, otherSchool) => {
+  const parsed = parseField(school);
+  return parsed.includes('* My school is not') ? parseField(otherSchool) : parsed;
+};
+
+const parseFieldOfStudy = (value) => {
+  const studies = parseField(value) || [];
+  const other = parseField(value, true);
+  if (other) studies.push(other);
+  return studies;
+};
+
+const parseTypeOfDegree = (value) => parseField(value) || parseField(value, true);
+
+const parseRoles = (value) => {
+  const applyingFor = parseField(value) || [];
+  const other = parseField(value, true);
+  if (other) applyingFor.push(other);
+  return applyingFor;
+};
+
+const parseHasLaptop = (value) => {
+  return parseField(value).includes('Yes');
+};
+
 class Student {
   constructor(student) {
     Object.keys(student).forEach((prop) => {
-      this[prop] = student[prop];
+      const value = student[prop];
+      if (prop === 'gender') this[prop] = parseGender(value);
+      else if (prop === 'school') this.school = parseNameOfSchool(value, student.otherSchool);
+      else if (prop === 'fieldOfStudy') this[prop] = parseFieldOfStudy(value);
+      else if (prop === 'typeOfDegree') this[prop] = parseTypeOfDegree(value);
+      else if (prop === 'applyingForRoles') this[prop] = parseRoles(value);
+      else if (prop === 'ownLaptop') this.hasLaptop = parseHasLaptop(value);
+      this[prop] = parseField(value);
     });
     this.id = student.id;
   }
 
-  getField(key, other = false) {
-    if (typeof this[key] === 'object' && this[key] !== null) {
-      if (other) return this[key].other;
-      return this[key].answer;
-    }
-    return this[key];
-  }
-
-  // Personal details
-  get firstName() {
-    return this.getField('firstName');
-  }
-
-  get lastName() {
-    return this.getField('firstName');
-  }
-
-  get email() {
-    return this.getField('email');
-  }
-
-  get phone() {
-    return this.getField('phone');
-  }
-
-  get gender() {
-    return this.getField('gender') || this.getField('gender', true);
-  }
-
-  get firstLanguage() {
-    return this.getField('firstLanguage');
-  }
-
-  get levelOfEnglish() {
-    return this.getField('levelOfEnglish');
-  }
-
-  // academia
-  get nameOfSchool() {
-    const school = this.getField('school');
-    return school.includes('* My school is not') ? this.getField('otherSchool') : school;
-  }
-
-  get fieldOfStudy() {
-    const studies = this.getField('fieldOfStudy') || [];
-    const other = this.getField('fieldOfStudy');
-    if (other) studies.push(other);
-    return studies;
-  }
-
-  get typeOfDegree() {
-    return this.getField('typeOfDegree') || this.getField('typeOfDegree', true);
-  }
-
-  get yearIntoDegree() {
-    return this.getField('typeOfDegree') || this.getField('typeOfDegree', true);
-  }
-
-  // experience
   get isAlum() {
-    return this.getField('alum').includes('Yes,');
+    return parseField(this.alum).includes('Yes,');
   }
 
   get wantsToCoach() {
-    return this.getField('alum').includes('Yes, but');
-  }
-
-  get cv() {
-    return this.getField('cv');
-  }
-
-  get portfolio() {
-    return this.getField('portfolio');
-  }
-
-  get projectMostProud() {
-    return this.getField('mostProud');
-  }
-
-  // motivation
-  get whyOsoc() {
-    return this.getField('whyOsoc');
-  }
-
-  get whyGoodFit() {
-    return this.getField('whyGoodFit');
-  }
-
-  get wantsToLearn() {
-    return this.getField('whatToLearn');
-  }
-
-  get whatSkills() {
-    return this.getField('whatSkills');
-  }
-
-  get applyingForRoles() {
-    const applyingFor = this.getField('applyingForRoles') || [];
-    const other = this.getField('fieldOfStudy');
-    if (other) applyingFor.push(other);
-    return applyingFor;
-  }
-
-  // practical
-  get canWorkUnderBelgianAgreement() {
-    return this.getField('canWorkUnderEmploymentAgreement');
-  }
-
-  get canWorkDuringHours() {
-    return this.getField('canWorkDuringHours');
-  }
-
-  get hasLaptop() {
-    return this.getField('ownLaptop').includes('Yes');
-  }
-
-  // socials
-  get linkedin() {
-    return this.getField('linkedin');
-  }
-
-  get github() {
-    return this.getField('github');
-  }
-
-  get twitter() {
-    return this.getField('twitter');
-  }
-
-  // rest
-  get anythingElseToShare() {
-    return this.getField('anythingElse');
+    return parseField(this.alum).includes('Yes, but');
   }
 }
 
