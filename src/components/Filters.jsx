bb54d6e5eae45c761ffa1e89cffc2d3ac48f8b8d
Michiel Leyman <michiel.leyman@gmail.com>
2020-04-21 11:54:48 +0200

    Temporarily remove filters

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import styles from '../assets/styles/dashboard.module.css';
 
 const Filters = () => {
-  return <header className={styles.filters}>Filters</header>;
+  return <header className={styles.filters} />;
 };
 
 export default Filters;
