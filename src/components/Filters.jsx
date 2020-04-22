623011abce43bb0e9a6baa944b8d4f302af49a28
Michiel Leyman <michiel.leyman@gmail.com>
2020-04-22 12:53:40 +0200

    Move filtering logic to Filters component

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,30 +1,57 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, memo } from 'react';
 import PropTypes from 'prop-types';
 import { Switch, SearchInput } from 'evergreen-ui';
+import { removeDiacritics } from '../util';
+import { Student } from '../models';
 import RoleFilter from './RoleFilter';
 
 import styles from '../assets/styles/filters.module.css';
+
 /*
 TODO:
 status filters (no official status, accepted, reject, maybe) - filterlist or segmented control
 status is locked-in - yes/no
 */
-const Filters = ({ setFilters }) => {
+const Filters = ({ students: studentObj, setFiltered }) => {
   const [searchQuery, setSearchQuery] = useState('');
   const [selectedRoles, setSelectedRoles] = useState([]);
   const [isAlum, setIsAlum] = useState(false);
   const [wantsToCoach, setWantsToCoach] = useState(false);
   const [includeAlreadySuggested, setIncludeAlreadySuggested] = useState(true);
 
-  useEffect(() => {
-    setFilters({
-      searchQuery,
-      selectedRoles,
-      isAlum,
-      wantsToCoach,
-      includeAlreadySuggested
+  const normalize = (str) => removeDiacritics(str.toLowerCase());
+
+  const students = Object.keys(studentObj)
+    .map((id) => studentObj[id])
+    .map((s) => {
+      s.firstNameNormalized = normalize(s.firstName);
+      s.lastNameNormalized = normalize(s.lastName);
+      return s;
     });
-  }, [searchQuery, selectedRoles, isAlum, wantsToCoach, includeAlreadySuggested]);
+
+  const filterBySearchQuery = (student) => {
+    const query = normalize(searchQuery);
+    const { firstNameNormalized, lastNameNormalized } = student;
+    return (
+      firstNameNormalized.includes(query) ||
+      lastNameNormalized.includes(query) ||
+      `${firstNameNormalized} ${lastNameNormalized}`.includes(query) ||
+      `${firstNameNormalized} ${lastNameNormalized}`.includes(query)
+    );
+  };
+
+  const sortByFirstNameThenLastName = (a, b) => {
+    if (a.firstNameNormalized < b.firstNameNormalized) return -1;
+    if (a.firstNameNormalized > b.firstNameNormalized) return 1;
+    if (a.lastNameNormalized < b.lastNameNormalized) return -1;
+    if (a.lastNameNormalized > b.lastNameNormalized) return 1;
+    return 0;
+  };
+
+  useEffect(() => {
+    setFiltered(students.filter(filterBySearchQuery).sort(sortByFirstNameThenLastName));
+  }, [students, searchQuery]);
+
   return (
     <header className={styles.filters}>
       <div>
@@ -51,14 +78,15 @@ const Filters = ({ setFilters }) => {
           checked={includeAlreadySuggested}
           onChange={(e) => setIncludeAlreadySuggested(e.target.checked)}
         />
-        <span>Include students I&apos;ve suggested for</span>
+        <span>Include students you&apos;ve suggested for</span>
       </div>
     </header>
   );
 };
 
 Filters.propTypes = {
-  setFilters: PropTypes.func.isRequired
+  setFiltered: PropTypes.func.isRequired,
+  students: PropTypes.objectOf(PropTypes.instanceOf(Student))
 };
 
-export default Filters;
+export default memo(Filters);
--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -1,53 +1,19 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Student } from '../models';
-import { removeDiacritics } from '../util';
 import Filters from './Filters';
 import StudentCard from './StudentCard';
 
 import styles from '../assets/styles/dashboard.module.css';
 
 const StudentList = ({ students }) => {
-  const [filters, setFilters] = useState({});
-
-  const lowerAndParseStr = (str) => removeDiacritics(str.toLowerCase());
-
-  const filterBySearchQuery = (student) => {
-    const query = lowerAndParseStr(filters.searchQuery);
-    const firstName = lowerAndParseStr(student.firstName);
-    const lastName = lowerAndParseStr(student.lastName);
-    return (
-      firstName.includes(query) ||
-      lastName.includes(query) ||
-      `${firstName} ${lastName}`.includes(query) ||
-      `${lastName} ${firstName}`.includes(query)
-    );
-  };
-
-  const sortByFirstNameThenLastName = (a, b) => {
-    const firstNameP1 = lowerAndParseStr(a.firstName);
-    const firstNameP2 = lowerAndParseStr(b.firstName);
-    const lastNameP1 = lowerAndParseStr(a.lastName);
-    const lastNameP2 = lowerAndParseStr(b.lastName);
-    if (firstNameP1 < firstNameP2) return -1;
-    if (firstNameP1 > firstNameP2) return 1;
-    if (lastNameP1 < lastNameP2) return -1;
-    if (lastNameP1 > lastNameP2) return 1;
-    return 0;
-  };
+  const [filtered, setFiltered] = useState([]);
 
   const renderStudent = (student) => <StudentCard key={student.id} student={student} />;
   return (
     <div className={styles['student-list']}>
-      <Filters setFilters={setFilters} />
-      <ol>
-        {students &&
-          Object.keys(students)
-            .map((id) => students[id])
-            .filter(filterBySearchQuery)
-            .sort(sortByFirstNameThenLastName)
-            .map(renderStudent)}
-      </ol>
+      <Filters students={students} setFiltered={setFiltered} />
+      <ol>{students && filtered.map(renderStudent)}</ol>
     </div>
   );
 };
