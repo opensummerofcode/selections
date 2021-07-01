13a6f1a7d2bf265183a3429fe8ea6390f1322430
Michiel Leyman <michiel.leyman@gmail.com>
2021-07-01 16:07:46 +0200

    build: set firebase project to playground

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import 'firebase/firestore';
 import 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDEUTpWJ8SFWZ0S7hzoLLFoIyKOgiBVqQ',
-  authDomain: 'osoc-selections-202.firebaseapp.com',
-  projectId: 'osoc-selections-202',
-  storageBucket: 'osoc-selections-202.appspot.com',
-  messagingSenderId: '983468915309',
-  appId: '1:983468915309:web:8face8aae1ac5cdef01d64'
+  apiKey: 'AIzaSyBKVUl4HYEm3gVLmqG0aOP8BjeMGfe3WWw',
+  authDomain: 'selections-playground.firebaseapp.com',
+  projectId: 'selections-playground',
+  storageBucket: 'selections-playground.appspot.com',
+  messagingSenderId: '445367300630',
+  appId: '1:445367300630:web:6400beb1254293c2217b37'
 };
 
 const fire = firebase.initializeApp(firebaseConfig);
