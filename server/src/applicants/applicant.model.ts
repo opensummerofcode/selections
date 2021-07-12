ec46d27b46589e35c296700277fc0d76f95c556f
Michiel Leyman <michiel.leyman@gmail.com>
2021-07-12 23:53:05 +0200

    feat: migrate and resolve applicant fields

--- server/src/applicants/applicant.model.ts
+++ server/src/applicants/applicant.model.ts
@@ -1,11 +1,21 @@
-import { ObjectType } from '@nestjs/graphql';
+import { ObjectType, Field, ID } from '@nestjs/graphql';
 import { IApplicant, ApplicantStatus } from 'common';
+import graphqlTypeJson from 'graphql-type-json';
 
 @ObjectType()
 export class Applicant implements IApplicant {
+  @Field(() => ID)
   id: string;
+
+  @Field()
   createdAt: Date;
+
+  @Field()
   updatedAt: Date;
+
+  @Field(() => graphqlTypeJson, { nullable: true })
   response: any;
+
+  @Field()
   status: ApplicantStatus;
 }
