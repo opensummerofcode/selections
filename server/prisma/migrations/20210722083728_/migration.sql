5ff4cc2535a11536bb995ffb3ef9389f876a1352
Michiel Leyman <michiel.leyman@gmail.com>
2021-07-22 10:38:03 +0200

    refactor: add project foreign key migration

---
+++ server/prisma/migrations/20210722083728_/migration.sql
@@ -0,0 +1,23 @@
+-- DropForeignKey
+ALTER TABLE "ApplicantsOnProjects" DROP CONSTRAINT "ApplicantsOnProjects_applicantId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "ApplicantsOnProjects" DROP CONSTRAINT "ApplicantsOnProjects_projectId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "UsersOnProjects" DROP CONSTRAINT "UsersOnProjects_projectId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "UsersOnProjects" DROP CONSTRAINT "UsersOnProjects_userId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "UsersOnProjects" ADD FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UsersOnProjects" ADD FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ApplicantsOnProjects" ADD FOREIGN KEY ("applicantId") REFERENCES "Applicant"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ApplicantsOnProjects" ADD FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
