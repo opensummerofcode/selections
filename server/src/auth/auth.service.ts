947ce19333ebcfca3b7255429385cca90f173521
Michiel Leyman <michiel.leyman@gmail.com>
2021-07-24 19:37:36 +0200

    chore: add auth module and service

---
+++ server/src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async validateUser(username: string, pass: string): Promise<any> {
+    // const user = await this.usersService.findOneById(username);
+    return null;
+  }
+}
