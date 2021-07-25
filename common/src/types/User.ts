794268b1140eec642ba5868ea2fb50a1858c2b35
Michiel Leyman <michiel.leyman@gmail.com>
2021-07-25 10:03:24 +0200

    refactor: make first and last name optional in favor of displayName

--- common/src/types/User.ts
+++ common/src/types/User.ts
@@ -1,11 +1,12 @@
-export type Role = 'USER' | 'COACH' | 'ADMIN'
+export type Role = 'USER' | 'COACH' | 'ADMIN';
 
 export interface IUser {
   readonly id: number;
-  readonly uuid: string;
+  readonly uuid?: string;
   email: string;
-  firstname: string;
-  lastname: string;
+  displayName: string;
+  firstname?: string;
+  lastname?: string;
   imageUrl?: string;
   role: Role;
   createdAt: Date;
