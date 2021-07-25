a76551a099da6ab98119ff87f912d44509876786
Michiel Leyman <michiel.leyman@gmail.com>
2021-07-25 20:52:16 +0200

    refactor: use named default exports for utils

--- client/src/util/sortByRole.js
+++ client/src/util/sortByRole.js
@@ -4,10 +4,12 @@ const roleWeighting = {
   pending: 1
 };
 
-export default (u1, u2) => {
+const sortByRole = (u1, u2) => {
   if (roleWeighting[u1.role] < roleWeighting[u2.role]) return 1;
   if (roleWeighting[u2.role] > roleWeighting[u1.role]) return -1;
   // sort alphabetically if roles are the same
   if (roleWeighting[u1.role] === roleWeighting[u2.role]) return u1.name > u2.name ? 1 : -1;
   return 0;
 };
+
+export default sortByRole;
