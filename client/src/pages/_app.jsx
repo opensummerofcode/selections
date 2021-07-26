0bdddbcba44c4f0bccc1c3952eea97e2b21c9eb2
Michiel Leyman <michiel.leyman@gmail.com>
2021-07-26 12:54:36 +0200

    feat(client): set up urql & normalized cache exchange

--- client/src/pages/_app.jsx
+++ client/src/pages/_app.jsx
@@ -1,9 +1,20 @@
 import Head from 'next/head';
 import Header from '@/components/Header';
 import { useAuth } from '@/services';
+import { API_URL } from '@/constants';
+import { Provider, Client, dedupExchange, fetchExchange } from 'urql';
+import { cacheExchange } from '@urql/exchange-graphcache';
 
 import 'normalize.css';
 import '@/assets/styles/index.css';
+
+// Use a normalized cache
+const cache = cacheExchange({});
+
+const client = new Client({
+  url: `${API_URL}/graphql`,
+  exchanges: [dedupExchange, cache, fetchExchange]
+});
 
 export default function App({ Component, pageProps }) {
   const { isLoading } = useAuth();
@@ -17,8 +28,10 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Selections | Open Summer of Code</title>
       </Head>
-      <Header />
-      {getLayout(<Component {...pageProps} />)}
+      <Provider value={client}>
+        <Header />
+        {getLayout(<Component {...pageProps} />)}
+      </Provider>
     </>
   );
 }
