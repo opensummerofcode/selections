7f91eace26ed23ae6c0d13ee56a4a9a2254ed7cf
Tischa <tischa.nyanguile@student.ehb.be>
2021-07-27 09:55:07 +0200

    Create migration seed

---
+++ server/prisma/migrations/20210727074858_init/migration.sql
@@ -0,0 +1,254 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'COACH', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('YES', 'MAYBE', 'NO');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "uuid" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "firstname" TEXT NOT NULL,
+    "lastname" TEXT NOT NULL,
+    "imageUrl" TEXT,
+    "role" "Role" NOT NULL DEFAULT E'USER',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" SERIAL NOT NULL,
+    "uuid" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "client" TEXT,
+    "templateUrl" TEXT,
+    "leadCoachId" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UsersOnProjects" (
+    "userId" INTEGER NOT NULL,
+    "projectId" INTEGER NOT NULL,
+    "assignedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY ("userId","projectId")
+);
+
+-- CreateTable
+CREATE TABLE "Applicant" (
+    "id" SERIAL NOT NULL,
+    "uuid" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "firstname" TEXT NOT NULL,
+    "lastname" TEXT NOT NULL,
+    "callname" TEXT,
+    "gender" TEXT NOT NULL,
+    "nationality" TEXT NOT NULL,
+    "addressId" INTEGER NOT NULL,
+    "phone" TEXT NOT NULL,
+    "isAlumni" BOOLEAN NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ApplicantsOnProjects" (
+    "applicantId" INTEGER NOT NULL,
+    "projectId" INTEGER NOT NULL,
+    "assignedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY ("applicantId","projectId")
+);
+
+-- CreateTable
+CREATE TABLE "Address" (
+    "id" SERIAL NOT NULL,
+    "addressLine" TEXT NOT NULL,
+    "postalCode" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "country" TEXT NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Suggestion" (
+    "id" SERIAL NOT NULL,
+    "status" "Status" NOT NULL,
+    "comment" TEXT NOT NULL,
+    "applicantId" INTEGER NOT NULL,
+    "suggesterId" INTEGER NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Profile" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "image_url" TEXT,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ApplicantProfile" (
+    "applicantId" INTEGER NOT NULL,
+    "profileId" INTEGER NOT NULL,
+
+    PRIMARY KEY ("applicantId","profileId")
+);
+
+-- CreateTable
+CREATE TABLE "ProjectProfile" (
+    "projectId" INTEGER NOT NULL,
+    "profileId" INTEGER NOT NULL,
+
+    PRIMARY KEY ("projectId","profileId")
+);
+
+-- CreateTable
+CREATE TABLE "Skill" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "SkillLevel" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ApplicantSkill" (
+    "applicantId" INTEGER NOT NULL,
+    "skillId" INTEGER NOT NULL,
+    "levelId" INTEGER NOT NULL,
+
+    PRIMARY KEY ("applicantId","skillId")
+);
+
+-- CreateTable
+CREATE TABLE "ProjectSkill" (
+    "projectId" INTEGER NOT NULL,
+    "skillId" INTEGER NOT NULL,
+
+    PRIMARY KEY ("projectId","skillId")
+);
+
+-- CreateTable
+CREATE TABLE "Answer" (
+    "id" SERIAL NOT NULL,
+    "text" TEXT NOT NULL,
+    "questionId" INTEGER NOT NULL,
+    "applicantId" INTEGER NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Question" (
+    "id" SERIAL NOT NULL,
+    "reference" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User.uuid_unique" ON "User"("uuid");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User.email_unique" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Project.uuid_unique" ON "Project"("uuid");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Applicant.uuid_unique" ON "Applicant"("uuid");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Applicant.email_unique" ON "Applicant"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Applicant.addressId_unique" ON "Applicant"("addressId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Skill.name_unique" ON "Skill"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "SkillLevel.name_unique" ON "SkillLevel"("name");
+
+-- AddForeignKey
+ALTER TABLE "Project" ADD FOREIGN KEY ("leadCoachId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UsersOnProjects" ADD FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UsersOnProjects" ADD FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Applicant" ADD FOREIGN KEY ("addressId") REFERENCES "Address"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ApplicantsOnProjects" ADD FOREIGN KEY ("applicantId") REFERENCES "Applicant"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ApplicantsOnProjects" ADD FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Suggestion" ADD FOREIGN KEY ("applicantId") REFERENCES "Applicant"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Suggestion" ADD FOREIGN KEY ("suggesterId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ApplicantProfile" ADD FOREIGN KEY ("applicantId") REFERENCES "Applicant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ApplicantProfile" ADD FOREIGN KEY ("profileId") REFERENCES "Profile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProjectProfile" ADD FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProjectProfile" ADD FOREIGN KEY ("profileId") REFERENCES "Profile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ApplicantSkill" ADD FOREIGN KEY ("applicantId") REFERENCES "Applicant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ApplicantSkill" ADD FOREIGN KEY ("skillId") REFERENCES "Skill"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ApplicantSkill" ADD FOREIGN KEY ("levelId") REFERENCES "SkillLevel"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProjectSkill" ADD FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProjectSkill" ADD FOREIGN KEY ("skillId") REFERENCES "Skill"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Answer" ADD FOREIGN KEY ("questionId") REFERENCES "Question"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Answer" ADD FOREIGN KEY ("applicantId") REFERENCES "Applicant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
